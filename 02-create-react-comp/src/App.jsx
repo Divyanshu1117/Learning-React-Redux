533d6fe4f1860b76f2fe436dc4ff09c1e1902572
Divyanshu1117 <vashishthdivyanshu18@gmail.com>
2026-07-15 00:24:58 +0530

    Initial commit

---
+++ 02-create-react-comp/src/App.jsx
@@ -0,0 +1,22 @@
+// import DVButton from './DVButton';
+// import {DVButton} from './DVButton';
+import Hello from './Hello';
+import Random from './Random';
+
+function App() {
+  return <div>
+    <h1>
+      This is the best Teact Course
+    </h1>
+    <Hello></Hello>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    {/* <DVButton>Click Me! DV</DVButton> */}
+  </div>
+}
+
+export default App;
---
+++ 02-create-react-comp/src/DVButton.jsx
@@ -0,0 +1,11 @@
+export function DVButton() {
+    return <button>Click Me! Bro</button>
+}
+
+// export default DVButton;
+
+// function DVButton() {
+//     return <button>Click Me! Bro</button>
+// }
+
+// export default DVButton;
