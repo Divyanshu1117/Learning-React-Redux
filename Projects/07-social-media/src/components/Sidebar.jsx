9bb8a4daa6d12470c72948d66eded2f1bcc641ca
Divyanshu1117 <vashishthdivyanshu18@gmail.com>
2026-08-01 02:32:41 +0530

    Add new features

--- Projects/07-social-media/src/components/Sidebar.jsx
+++ Projects/07-social-media/src/components/Sidebar.jsx
@@ -1,4 +1,7 @@
-const Sidebar = () => {
+import PropTypes from "prop-types";
+
+const Sidebar = ({ selectedTab, setSelectedTab }) => {
+
     return (
         <div
             className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
@@ -22,8 +25,8 @@ const Sidebar = () => {
             <hr />
 
             <ul className="nav nav-pills flex-column mb-auto">
-                <li className="nav-item">
-                    <a href="#" className="nav-link active" aria-current="page">
+                <li className="nav-item" onClick={() => { setSelectedTab("Home") }}>
+                    <a href="#" className={`nav-link text-white ${selectedTab === 'Home' && 'active'}`} aria-current="page">
                         <svg
                             className="bi pe-none me-2"
                             width="16"
@@ -36,8 +39,8 @@ const Sidebar = () => {
                     </a>
                 </li>
 
-                <li>
-                    <a href="#" className="nav-link text-white">
+                <li onClick={() => { setSelectedTab("Create Post") }}>
+                    <a href="#" className={`nav-link text-white ${selectedTab === 'Create Post' && 'active'}`}>
                         <svg
                             className="bi pe-none me-2"
                             width="16"
@@ -99,6 +102,11 @@ const Sidebar = () => {
             </div>
         </div>
     );
+};
+
+Sidebar.propTypes = {
+    selectedTab: PropTypes.string.isRequired,
+    setSelectedTab: PropTypes.func.isRequired,
 };
 
 export default Sidebar;
